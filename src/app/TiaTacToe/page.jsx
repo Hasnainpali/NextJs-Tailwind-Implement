e2b28ec69bba173804f1d6aeba0cdb5694267fbc
Hasnainpali <hasnainpali321@gmail.com>
2024-03-14 14:57:39 +0500

    Implemnet TailwindCSS

---
+++ src/app/TiaTacToe/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TiaTacTae from '../components/TiaTacTae'
+import Navbar from '../components/Navbar'
+
+
+export default function page() {
+  return (
+    <div>
+       <Navbar/>
+       <TiaTacTae/>
+    </div>
+  )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+import TiaTacTae from "./components/TiaTacTae";
+
+
+export default function Home() {
+  return (
+    <main >
+      <Navbar/>
+    </main>
+  );
+}
